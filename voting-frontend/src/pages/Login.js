9145f92663bc6e24613b31be98477d04971dcc65
keerthana2624 <keerthanamummy2002@gmail.com>
2025-05-22 14:42:12 +0530

    Implimented the login page logic

---
+++ voting-frontend/src/pages/Login.js
@@ -0,0 +1,37 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import API from '../api/axios';
+
+const Login = () => {
+  const [formData, setFormData] = useState({ email: '', password: '' });
+  const navigate = useNavigate();
+
+  const handleChange = (e) =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await API.post('/auth/login', formData);
+      localStorage.setItem('token', res.data.token);
+      alert('Login successful!');
+      navigate('/elections');
+    } catch (error) {
+      alert('Login failed!');
+    }
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <form onSubmit={handleSubmit}>
+        <input name="email" type="email" placeholder="Email" onChange={handleChange} required />
+        <input name="password" type="password" placeholder="Password" onChange={handleChange} required />
+        <button type="submit">Login</button>
+      </form>
+      <p>Don’t have an account? <a href="/register">Register here</a></p>
+    </div>
+  );
+};
+
+export default Login;
