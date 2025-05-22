5821b97d9f7e80659cd78840e5a66c9e2cd431f8
keerthana2624 <keerthanamummy2002@gmail.com>
2025-05-22 14:39:39 +0530

    created the comonents and api folders

---
+++ voting-frontend/src/api/axios.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const API = axios.create({
+  baseURL: 'http://localhost:5000/api', // Replace with your backend URL
+});
+
+// Add the token to every request if user is logged in
+API.interceptors.request.use((req) => {
+  const token = localStorage.getItem('token');
+  if (token) {
+    req.headers.Authorization = `Bearer ${token}`;
+  }
+  return req;
+});
+
+export default API;
